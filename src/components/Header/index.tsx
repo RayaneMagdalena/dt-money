dd21f6e1403c5ed6d2bc30bee94e286e3cb91eb2
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2024-05-03 18:53:34 -0300

    NewTransactionModal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from '../../assets/logo.svg'
+import logoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
-    return (
-        <div>
-            <HeaderContainer>
-                <HeaderContent>
-                    <img src={logoImg} alt="" />
+  return (
+    <div>
+      <HeaderContainer>
+        <HeaderContent>
+          <img src={logoImg} alt="" />
 
-                    <NewTransactionButton>Nova Transação</NewTransactionButton>
-                </HeaderContent>
-            </HeaderContainer>
-        </div>
-    )
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+        <NewTransactionModal />
+          </Dialog.Root>
+        </HeaderContent>
+      </HeaderContainer>
+    </div>
+  );
 }
