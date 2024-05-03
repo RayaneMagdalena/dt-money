38049d1ae1f6e1195a5fd735b6f5e85c7da2d1ec
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2024-05-03 15:42:34 -0300

    SearchForm component

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,22 +1,27 @@
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 
 export function Transactions() {
-    return (
-        <div>
-            <Header />
-            <Summary />
+  return (
+    <div>
+      <Header />
+      <Summary />
 
-            <TransactionsContainer>
+      <TransactionsContainer>
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
               <td width="50%">Desenvolvimento de site</td>
               <td>
-                <PriceHighlight variant="income">
-                  R$ 12.000,00
-                </PriceHighlight>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
               </td>
               <td>Venda</td>
               <td>13/04/2022</td>
@@ -24,9 +29,7 @@ export function Transactions() {
             <tr>
               <td width="50%">Hambúrguer</td>
               <td>
-                <PriceHighlight variant="outcome">
-                  -R$ 59,00
-                </PriceHighlight>
+                <PriceHighlight variant="outcome">-R$ 59,00</PriceHighlight>
               </td>
               <td>Alimentação</td>
               <td>10/04/2022</td>
@@ -34,6 +37,6 @@ export function Transactions() {
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
-        </div>
-    )
+    </div>
+  );
 }
