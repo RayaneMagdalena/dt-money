65a9d21b3db60806ff766f1207b1ed88bb50f407
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2024-05-04 17:26:24 -0300

    react-hook-form configuration

--- src/pages/transactions/components/SearchForm/index.tsx
+++ src/pages/transactions/components/SearchForm/index.tsx
@@ -1,12 +1,45 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+// Schema - define tipagem e regras de validação
+const searchFormSchema = z.object({
+  // nome do input - o mesmo que esta em register
+  query: z.string(), 
+});
+
+// Crio a tipagem usando o Schema
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
+
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+  const { 
+    register, 
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<SearchFormInputs>({
+    // Resolver faz a integração do hook-form com o zod.
+    resolver: zodResolver(searchFormSchema),
+  });
 
-      <button type="submit">
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(data);
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+
+      <input 
+      type="text" 
+      placeholder="Busque por transações" 
+      {...register('query')}
+      />
+
+<button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
